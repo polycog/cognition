ba8604be30bf51c091b750eb874228dd1f6a0430
Nate Derbinsky <nate.derbinsky@gmail.com>
2026-05-28 13:23:44 -0400

    documented functypes

--- src/cognition/functypes.py
+++ src/cognition/functypes.py
@@ -10,12 +10,25 @@ from collections.abc import Callable
 #
 
 type Supplier[X] = Callable[[], X]
+"""Represents a supplier of results."""
+
 type Function[X, Y] = Callable[[X], Y]
+"""Represents a function that accepts one argument and produces a result."""
+
 type BiFunction[X, Y, Z] = Callable[[X, Y], Z]
+"""Represents a function that accepts two arguments and produces a result."""
+
 type TriFunction[X, Y, Z, Q] = Callable[[X, Y, Z], Q]
+"""Represents a function that accepts three arguments and produces a result."""
 
 type Consumer[X] = Function[X, None]
+"""Represents an operation that accepts a single input argument and returns no result."""
+
 type BiConsumer[X, Y] = BiFunction[X, Y, None]
+"""Represents an operation that accepts two input arguments and returns no result."""
 
 type Predicate[X] = Function[X, bool]
+"""Represents a predicate (boolean-valued function) of one argument."""
+
 type BiPredicate[X, Y] = BiFunction[X, Y, bool]
+"""Represents a predicate (boolean-valued function) of two arguments."""
