6ea6824f545695500b2f8abb76a6fb97a2910e29
Nate Derbinsky <nate.derbinsky@gmail.com>
2026-07-08 16:55:01 -0400

    small rename

--- src/cognition/__init__.py
+++ src/cognition/__init__.py
@@ -9,7 +9,7 @@ from .utility import *  # noqa: F403
 
 from .core import *  # noqa: F403
 
-from .convenience import *  # noqa: F403
+from .enhancements import *  # noqa: F403
 
 from .chain import *  # noqa: F403
 
