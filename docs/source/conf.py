acb206695fbc747d6284eb5c07631e4ff2805af8
Shiwali Mohan <shiwali.mohan@gmail.com>
2026-09-13 16:26:13 -0700

    fixed the static name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Cognition'
+project = 'cognition'
 copyright = '2026, Polycog, Inc.'
 author = 'Polycog, Inc.'
 
